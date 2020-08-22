a2bdb6675aeebc43f642de4c1326c3bf92bd8b88
WayneGreat <weihongye669@gmail.com>
2020-08-22 11:45:10 +0800

    package

--- Python常用模块/Package1.py
+++ Python常用模块/Package1.py
@@ -1,3 +1,22 @@
-#coding-utf8
+# coding-utf8
+
+class Book:
+    lang = 'learn python with wayne'
+
+    def __init__(self, author):
+        self.author = author
+
+    def get_name(self):
+        return self.author
 
 
+def new_book():
+    return "数据准备和特征工程"
+
+
+if __name__ == '__main__':  # 程序入口
+    python = Book('wayne')
+    author_name = python.get_name()
+    print(author_name)
+    published = new_book()
+    print(published)
